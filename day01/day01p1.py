addf20ba53a16e85df137a980a85df467b99fc09
Jurph <j.r.parsons@gmail.com>
2024-12-01 08:29:50 -0500

    Passes tests and passes validation

--- day01/day01p1.py
+++ day01/day01p1.py
@@ -7,7 +7,8 @@
 # 2. Sort both lists ascending  
 # 3. The distance between them (absolute value) should be added to a sum 
 
-
+import math
+import itertools
 from dataclasses import dataclass
 
 # Problem() is my class that ingests the day's input and structures it for easy computation
@@ -18,14 +19,31 @@ class Problem():
         rightintegers = []
         lines = open(filename).read().splitlines()
         for line in lines:
-            pairs.append(line.split())
+            pairs.append(line.split("   "))
         leftintegers, rightintegers = list(zip(*pairs))
         leftintegers = list(map(int, leftintegers))
         rightintegers = list(map(int, rightintegers))
         self.leftintegers = leftintegers
         self.rightintegers = rightintegers
+        # self.sum = self.span()
         return
     
+    def span(self):
+        sum = 0
+        lefty = sorted(self.leftintegers)
+        righty = sorted(self.rightintegers)
+        for (l, r) in zip(lefty, righty):
+            sum += abs(l-r)
+        return sum
+
+
+def main():
+    # Ingest and format the data
+    t = Problem("C:\\Users\\Jurph\\Documents\\Python Scripts\\aoc2024\\day01\\test.txt")
+    p = Problem("C:\\Users\\Jurph\\Documents\\Python Scripts\\aoc2024\\day01\\input.txt")  
+
+    print("Test sum is {}".format(t.span()))
+    print("Real sum is {}".format(p.span()))
+
 if __name__ == "__main__":
-    p = Problem("test.txt")
-    print(p.leftintegers, p.rightintegers)
+    main()
