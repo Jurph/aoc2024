7b705cb66d1556bf3739cba51d6440f3e1be0fdd
Jurph <j.r.parsons@gmail.com>
2024-12-01 08:12:55 -0500

    updated file ingest

--- day01/day01p1.py
+++ day01/day01p1.py
@@ -13,18 +13,19 @@ from dataclasses import dataclass
 # Problem() is my class that ingests the day's input and structures it for easy computation
 class Problem():
     def __init__(self, filename):
-        integers = []
-        strings = [] 
         pairs = []
+        leftintegers = []
+        rightintegers = []
         lines = open(filename).read().splitlines()
         for line in lines:
-            strings.append(line)
             pairs.append(line.split())
-            try:
-                integers.append(int(line))
-            except(ValueError):
-                integers.append(int(0))
-        self.strings = strings
-        self.integers = integers
+        leftintegers, rightintegers = list(zip(*pairs))
+        leftintegers = list(map(int, leftintegers))
+        rightintegers = list(map(int, rightintegers))
+        self.leftintegers = leftintegers
+        self.rightintegers = rightintegers
         return
     
+if __name__ == "__main__":
+    p = Problem("test.txt")
+    print(p.leftintegers, p.rightintegers)
